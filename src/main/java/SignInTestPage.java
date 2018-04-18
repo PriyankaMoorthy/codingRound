536d3a9f1b3fca3c99125ef6e8c8e00eeb48c3cb
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-18 12:52:02 +0530

    Update SignInTestPage.java

--- src/main/java/SignInTestPage.java
+++ src/main/java/SignInTestPage.java
@@ -4,5 +4,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SignInTestPage {
+  
+  @FindBy (linkText = "Your trips")
+  private static WebElement Yourtripslink;
+  @FindBy (id = "SignIn")
+  private static WebElement signIn;
+  @FindBy (id = "signInButton")
+  private static WebElement signInButton;
+  @FindBy (id = "errors1")
+  private static WebElement ErrorText;
+  
+        String errors1 = driver.findElement(By.id("errors1")).getText();
+        Assert.assertTrue(errors1.contains("There were errors in your submission"));
 
 }
