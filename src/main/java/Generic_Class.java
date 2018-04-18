1ba0dc981e02c11a30ce025884c278dd0d1e8904
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-18 12:18:22 +0530

    Update Generic_Class.java

--- src/main/java/Generic_Class.java
+++ src/main/java/Generic_Class.java
@@ -2,26 +2,18 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class Generic_Class 
-{
-
-	
+{	
 	//read data from Property file
-
 	public static String getPropertyValue(String propKey) 
 	{
-
 		Properties config = null;
 		try {
 			config = new Properties();
 			FileInputStream ip = new FileInputStream("./Resources/config.properties");
-
 			config.load(ip);
 		} catch (Exception e) {
 
-
 		}
-
 		return config.getProperty(propKey);
-
 	}
 }
