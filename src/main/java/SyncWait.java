5db0e61b886100acc9d44779efdeea9aefc8347d
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-18 12:26:47 +0530

    Create SyncWait.java

---
+++ src/main/java/SyncWait.java
@@ -0,0 +1,10 @@
+public class SyncWait {
+  
+public static void waitFor(int durationInMilliSeconds) {
+        try {
+            Thread.sleep(durationInMilliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+}
