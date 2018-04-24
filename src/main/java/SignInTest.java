dbeac995aa7cb2dd66301d13b6f5dae0a1cfd19f
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-24 09:21:32 +0530

    Update SignInTest.java

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -8,10 +8,15 @@ import org.testng.annotations.Test;
 public class SignInTest extends BrowserFactory{
 
     @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+    public void shouldThrowAnErrorIfSignInDetailsAreMissing() throws InterruptedException {
 
         SyncWait.waitFor(2000);
-
+        Generic_Class gc= new Generic_Class();
+        
+        SignInTestPage clearTrip = new SignInTestPage(driver);
+        
+        clearTrip.
+        
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
 
