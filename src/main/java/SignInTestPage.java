479f9abce11bf0e529ff11889c11ad81a0d38cdb
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-24 09:29:52 +0530

    Update SignInTestPage.java

--- src/main/java/SignInTestPage.java
+++ src/main/java/SignInTestPage.java
@@ -2,9 +2,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class SignInTestPage {
   
+  WebDriver driver;
+	
   @FindBy (linkText = "Your trips")
   private WebElement yourTripsLink;
   @FindBy (id = "SignIn")
@@ -13,6 +16,12 @@ public class SignInTestPage {
   private WebElement signInButton;
   @FindBy (id = "errors1")
   private WebElement errorText;
+	
+  public SignInTestPage(WebDriver driver)
+  {
+     this.driver=driver;
+     PageFactory.initElements(driver, this);
+  }
   
   public void get_yourtripslinktext()
   {
