beb24bc7b8db7acf1ce5e4517a6d1c7f9702dd34
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-25 08:40:53 +0530

    Update HotelBookingTestPage.java

--- src/main/java/HotelBookingTestPage.java
+++ src/main/java/HotelBookingTestPage.java
@@ -1,1 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+
+      @FindBy(linkText = "Hotels")
+	    private WebElement hotelLink;
+
+	    @FindBy(id = "Tags")
+	    private WebElement localityTextBox;
+
+	    @FindBy(id = "SearchHotelsButton")
+	    private WebElement searchButton;
+
+	    @FindBy(id = "travellersOnhome")
+	    private WebElement travellerSelection;
