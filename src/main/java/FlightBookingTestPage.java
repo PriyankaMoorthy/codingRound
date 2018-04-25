2e4867ffbc9773b885eadd8149662f6d1575a52f
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-25 15:52:04 +0530

    Update FlightBookingTestPage.java

--- src/main/java/FlightBookingTestPage.java
+++ src/main/java/FlightBookingTestPage.java
@@ -7,11 +7,61 @@ import org.openqa.selenium.support.PageFactory;
 public class FlightBookingTestPage {
   
   WebDriver driver;
-
+  
+  @FindBy (id = "OneWay")
+  private WebElement selectOneWayRadioBtn;
+  @FindBy (id = "FromTag")
+  private WebElement fromTagTextBox;
+  @FindBy (id = "toTag")
+  private WebElement toTagTextBox;
+  @FindBy (id = "SearchBtn")
+  private WebElement searchBtn;
+  @FindBy (xpath = "//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")
+  private WebElement datePicker;
+  
   public FlightBookingTestPage(WebDriver driver)
   {
      this.driver=driver;
      PageFactory.initElements(driver, this);
   }
   
+  public void get_OneWayRadioBtnClick()
+  {
+	   selectOneWayRadioBtn.click();
+  }
+  
+  public void get_FromTagTextBoxClick()
+  {
+	   fromTagTextBox.click();
+  }
+  
+  public void get_FromTagTextBoxClick()
+  {
+	   fromTagTextBox.click();
+  }
+  
+  public void get_FromTagTextBox(String fromText)
+  {
+	   fromTagTextBox.sendKeys(fromText);
+  }
+  
+  public void get_ToTagTextBoxClick()
+  {
+	   toTagTextBox.click();
+  }
+  
+  public void get_ToTagTextBox(String toText)
+  {
+	   toTagTextBox.sendKeys(toText);
+  }
+  
+  public void get_DatePicker()
+  {
+	   datePicker.click();
+  }
+  
+  public void get_ClickSearchBtn()
+  {
+	   searchBtn.click();
+  }
   
