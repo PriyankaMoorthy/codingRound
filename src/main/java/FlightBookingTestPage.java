2e44813570a5d7dfd62d78328d9b6f526ba47a1a
PriyankaMoorthy <37620643+PriyankaMoorthy@users.noreply.github.com>
2018-04-26 11:14:01 +0530

    Update FlightBookingTestPage.java

--- src/main/java/FlightBookingTestPage.java
+++ src/main/java/FlightBookingTestPage.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import com.sun.javafx.PlatformUtil;
+import org.openqa.selenium.support.ui.Select;
 
 public class FlightBookingTestPage {
   
